db17e27c98742198c898041fde9dcf8483dd64f3
Ivo van Doesburg <vandoesburg@gmail.com>
2012-06-26 13:34:58 +0200

    it works! oh well, never mind

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, Markup, jsonify
 import json
 import os
-
+from imdb import IMDb
 
 app = Flask(__name__)
 
@@ -26,6 +26,26 @@ if os.path.exists('settings_local.py'):
 def index():
     template_args = {}
     return render_template('index.html', **template_args)
+
+
+@app.route("/search")
+def search():
+    title = request.args.get('q', '')
+    ia = IMDb()
+    movies = ia.search_movie(title)
+    results = []
+    for movie in movies:
+        # print dir(result)
+        # print movie['long imdb title']
+        results.append({'value': movie.movieID, 'name': movie['long imdb title']})
+    # print results
+    return json.dumps(results)
+
+
+@app.route("/save")
+def save():
+    favs = request.args.get('favs', '')
+    return render_template('save.html', {'favs': favs})
 
 
 app.debug = app.config['DEBUG']
